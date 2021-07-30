dc703a535e8657b5fce862b351f3481233690e21
SagarNeelgar <sagarneelgar@gmail.com>
2021-07-30 20:04:15 +0530

    Create a react app

---
+++ build/precache-manifest.cfb7b4558476dd45335fcc08fc42b322.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99065e00da86a94d8e3be667bb2aa18",
+    "url": "/Keeper_App/index.html"
+  },
+  {
+    "revision": "599f727a25224ca4cb61",
+    "url": "/Keeper_App/static/js/2.14ebd957.chunk.js"
+  },
+  {
+    "revision": "4c28e42c79d0fcbe18b4",
+    "url": "/Keeper_App/static/js/main.cc64e5c0.chunk.js"
+  },
+  {
+    "revision": "08392bd83b0bd60e8cf9",
+    "url": "/Keeper_App/static/js/runtime-main.12534e6e.js"
+  }
+]);
